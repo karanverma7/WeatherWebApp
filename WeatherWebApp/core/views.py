beec8bef908a353241108aacfc2acc0c2705a225
karanverma7 <karanverma1601@gmail.com>
2020-09-10 21:38:40 +0530

    Added 404 error page

--- WeatherWebApp/core/views.py
+++ WeatherWebApp/core/views.py
@@ -4,10 +4,24 @@ import requests
 
 def index(request):
 
+    city = 'Delhi'
+    # Checking the form input
+
     if request.POST:
-        city = request.POST['city']
-        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=7f58fb81d202f06c8b815b95b72d4473'
-        r = requests.get(url.format(city)).json()
+        city = request.POST['city']        
+
+    # API request to get the Weather information
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=7f58fb81d202f06c8b815b95b72d4473'
+    r = requests.get(url.format(city)).json()
+
+    # API request to get the Location image
+
+    img_url = 'https://api.unsplash.com/search/photos?per_page=1&query={}&client_id=jKdZP3hygPsn8nZa0u26Kqkq59BhjQGDconnica895c'
+    r2 = requests.get(img_url.format(city)).json()
+
+    if r['cod'] == 200 and r2['total'] != 0:
+
         temp_in_celcius = "{:.2f}".format((r['main']['temp'] - 32) * 5/9)
 
         city_weather = {
@@ -20,11 +34,12 @@ def index(request):
             'description': r['weather'][0]['main'],
             'icon': r['weather'][0]['icon']
         }
-
-        img_url = 'https://api.unsplash.com/search/photos?per_page=1&query={}&client_id=jKdZP3hygPsn8nZa0u26Kqkq59BhjQGDconnica895c'
-        r2 = requests.get(img_url.format(city)).json()
+        
         img_src = r2['results'][0]['urls']['thumb']
 
         return render(request, 'index.html', {'city_weather': city_weather, 'img_src': img_src})
+    
     else:
-        return render(request, 'index.html')
+        message = "City not found"
+        return render(request, 'index.html', {'message': message})
+
